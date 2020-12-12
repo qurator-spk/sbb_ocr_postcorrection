07b46e4fea9092d076f5613cffa339b8e362f5e4
Robin Schaefer <robin.schaefer@posteo.de>
2020-12-12 17:29:19 +0100

    Change module paths from 'sbb_ocr_correction' to 'sbb_ocr_postcorrection'

--- setup.py
+++ setup.py
@@ -20,24 +20,24 @@ setup(
     entry_points={
         'console_scripts': [
             #cli_preproc.py
-            'align-sequences=qurator.sbb_ocr_correction.preprocessing.cli_preproc:align_sequences',
-            'apply-sliding-window=qurator.sbb_ocr_correction.preprocessing.cli_preproc:apply_sliding_window',
-            'create-detector-targets=qurator.sbb_ocr_correction.preprocessing.cli_preproc:create_detector_targets',
-            'filter-language=qurator.sbb_ocr_correction.preprocessing.cli_preproc:filter_language',
-            'parse-xml=qurator.sbb_ocr_correction.preprocessing.cli_preproc:parse_xml',
-            'split-dataset=qurator.sbb_ocr_correction.preprocessing.cli_preproc:split_dataset',
-            'split-dataset-sliding-window=qurator.sbb_ocr_correction.preprocessing.cli_preproc:split_dataset_sliding_window',
+            'align-sequences=qurator.sbb_ocr_postcorrection.preprocessing.cli_preproc:align_sequences',
+            'apply-sliding-window=qurator.sbb_ocr_postcorrection.preprocessing.cli_preproc:apply_sliding_window',
+            'create-detector-targets=qurator.sbb_ocr_postcorrection.preprocessing.cli_preproc:create_detector_targets',
+            'filter-language=qurator.sbb_ocr_postcorrection.preprocessing.cli_preproc:filter_language',
+            'parse-xml=qurator.sbb_ocr_postcorrection.preprocessing.cli_preproc:parse_xml',
+            'split-dataset=qurator.sbb_ocr_postcorrection.preprocessing.cli_preproc:split_dataset',
+            'split-dataset-sliding-window=qurator.sbb_ocr_postcorrection.preprocessing.cli_preproc:split_dataset_sliding_window',
             #cli_feature.py
-            'create-encoding-mapping=qurator.sbb_ocr_correction.feature_extraction.cli_feature:create_encoding_mapping',
-            'encode-features=qurator.sbb_ocr_correction.feature_extraction.cli_feature:encode_features',
-            'encode-features-hack=qurator.sbb_ocr_correction.feature_extraction.cli_feature:encode_features_hack',
+            'create-encoding-mapping=qurator.sbb_ocr_postcorrection.feature_extraction.cli_feature:create_encoding_mapping',
+            'encode-features=qurator.sbb_ocr_postcorrection.feature_extraction.cli_feature:encode_features',
+            'encode-features-hack=qurator.sbb_ocr_postcorrection.feature_extraction.cli_feature:encode_features_hack',
             #cli_correct.py
-            'evaluate-detector=qurator.sbb_ocr_correction.mt.cli_correct:evaluate_detector',
-            'evaluate-translator=qurator.sbb_ocr_correction.mt.cli_correct:evaluate_translator',
-            'predict-detector=qurator.sbb_ocr_correction.mt.cli_correct:predict_detector',
-            'predict-translator=qurator.sbb_ocr_correction.mt.cli_correct:predict_translator',
-            'train-detector=qurator.sbb_ocr_correction.mt.cli_correct:train_detector',
-            'train-translator=qurator.sbb_ocr_correction.mt.cli_correct:train_translator'
+            'evaluate-detector=qurator.sbb_ocr_postcorrection.mt.cli_correct:evaluate_detector',
+            'evaluate-translator=qurator.sbb_ocr_postcorrection.mt.cli_correct:evaluate_translator',
+            'predict-detector=qurator.sbb_ocr_postcorrection.mt.cli_correct:predict_detector',
+            'predict-translator=qurator.sbb_ocr_postcorrection.mt.cli_correct:predict_translator',
+            'train-detector=qurator.sbb_ocr_postcorrection.mt.cli_correct:train_detector',
+            'train-translator=qurator.sbb_ocr_postcorrection.mt.cli_correct:train_translator'
         ]
     }
 )
