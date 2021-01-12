10a9e883b17da274f0a4806c7751661f01d8298e
Robin Schaefer <robin.schaefer@posteo.de>
2021-01-12 12:59:55 +0100

    Add OCRCorrectionDataset to data_structures.py

--- qurator/sbb_ocr_postcorrection/data_structures.py
+++ qurator/sbb_ocr_postcorrection/data_structures.py
@@ -4,6 +4,8 @@ from collections import defaultdict
 import os
 import pandas as pd
 import sqlite3
+import torch
+from torch.utils.data import Dataset
 
 class Corpus():
     def __init__(self, pre_init_corpus=None):
@@ -249,3 +251,17 @@ class Corpus():
         #    rows_as_df = pd.read_sql_query('SELECT * from ' + table + ';', conn)
 
         self.aligned_sequences = rows
+
+class OCRCorrectionDataset(Dataset):
+    '''Dataset containing OCR and GT for OCR post-correction.'''
+
+    def __init__(self, ocr_encodings, gt_encodings):
+        self.ocr_encodings = ocr_encodings
+        self.gt_encodings = gt_encodings
+        assert ocr_encodings.shape == gt_encodings.shape, 'OCR and GT need to have the same dimensions.'
+
+    def __len__(self):
+        return len(self.ocr_encodings)
+
+    def __getitem__(self, idx):
+        return torch.tensor([self.ocr_encodings[idx], self.gt_encodings[idx]])
