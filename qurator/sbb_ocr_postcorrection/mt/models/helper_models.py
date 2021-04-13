5741f3682b75c0999f6d5684858306a3c6377639
RobinSchaefer <robin.schaefer@posteo.de>
2021-04-13 11:24:05 +0200

    Update helper_models.py

--- qurator/sbb_ocr_postcorrection/mt/models/helper_models.py
+++ qurator/sbb_ocr_postcorrection/mt/models/helper_models.py
@@ -10,18 +10,33 @@ class ArgMaxConverter(nn.Module):
         output_size = input_size 
         self.output_size = output_size
 
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, hidden_size)
-        self.fc3 = nn.Linear(hidden_size, output_size)
-        self.relu = nn.ReLU()
+        self.fc1 = nn.Linear(input_size, hidden_size*2)
+        self.fc2 = nn.Linear(hidden_size*2, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, hidden_size*2)
+        self.fc4 = nn.Linear(hidden_size*2, output_size)
+        self.relu = nn.LeakyReLU(negative_slope=0.01)
         self.softmax = nn.Softmax(dim=2)
-    
+        self.dropout = nn.Dropout(0.2) 
+        self.dropout_hidden = nn.Dropout(0.5)
+   
     def forward(self, x):
 
-        x = self.relu(self.fc1(x))
-        x = self.relu(self.fc2(x))
-        x = self.fc3(x)
 
-        #import pdb; pdb.set_trace()
+        x = self.dropout(x)
+        x = self.fc1(x)
+        x = self.relu(x)
+        
+        x = self.dropout_hidden(x)
+        x = self.fc2(x)
+        x = self.relu(x)
+        
+        x = self.dropout_hidden(x)
+        x = self.fc3(x)
+        x = self.relu(x)
+        
+        x = self.dropout(x)
+        x = self.fc4(x)
+
+        x = self.softmax(x)
 
         return x
