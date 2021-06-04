a3adff4473b64006ef843e2e652068307cb8bf05
Robin Schaefer <robin.schaefer@posteo.de>
2021-06-04 11:44:02 +0200

    Update Correction Dataset to make GT optional

--- qurator/sbb_ocr_postcorrection/data_structures.py
+++ qurator/sbb_ocr_postcorrection/data_structures.py
@@ -255,13 +255,18 @@ class Corpus():
 class OCRCorrectionDataset(Dataset):
     '''Dataset containing OCR and GT for OCR post-correction.'''
 
-    def __init__(self, ocr_encodings, gt_encodings):
+    def __init__(self, ocr_encodings, gt_encodings=None):
         self.ocr_encodings = ocr_encodings
         self.gt_encodings = gt_encodings
-        assert ocr_encodings.shape == gt_encodings.shape, 'OCR and GT need to have the same dimensions.'
+        if gt_encodings is not None:
+            self.gt_encodings = gt_encodings
+            assert ocr_encodings.shape == gt_encodings.shape, 'OCR and GT need to have the same dimensions.'
 
     def __len__(self):
         return len(self.ocr_encodings)
 
     def __getitem__(self, idx):
-        return torch.tensor([self.ocr_encodings[idx], self.gt_encodings[idx]])
+        if self.gt_encodings is not None:
+            return torch.tensor([self.ocr_encodings[idx], self.gt_encodings[idx]])
+        else:
+            return torch.tensor([self.ocr_encodings[idx]])
